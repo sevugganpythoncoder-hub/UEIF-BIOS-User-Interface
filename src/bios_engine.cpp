b3a48c26bd2466d85c20890df4b8bb746df985d6
Python-Mediator <sevugganpythoncoder@gmail.com>
2026-06-18 19:35:09 +0530

    Update bios_engine.cpp

--- src/bios_engine.cpp
+++ src/bios_engine.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <cstring>
 
 #ifdef _WIN32
     #include <windows.h>
@@ -10,48 +11,44 @@
 #endif
 
 struct AsusBiosMemoryMap {
-    char cpuModel[64] = "Detecting...";
-    uint32_t ramSpeedMhz = 4800;
-    uint32_t totalMemoryMb = 0;
-    float cpuTemperatureC = 0.0f;
-    float cpuCoreVoltageV = 0.000f;
-    uint8_t aiOverclockMode = 0;   
-    uint8_t xmpProfileStatus = 0;  
-    uint8_t intelRstEnabled = 1;   
-    uint16_t bootPrioritySequence[3] = {1, 2, 3}; 
-    uint32_t manualFanCurvePoints[4] = {20, 40, 70, 100}; 
-    uint32_t bclkFrequency = 100;
-    uint32_t cpuRatio = 54;
-    uint8_t secureBootState = 1;
-    uint8_t hyperThreadingState = 1;
-    uint8_t virtualizationState = 1;
+    char cpuModel[64];
+    uint32_t ramSpeedMhz;
+    uint32_t totalMemoryMb;
+    float cpuTemperatureC;
+    float cpuCoreVoltageV;
+    uint8_t aiOverclockMode;   
+    uint8_t xmpProfileStatus;  
+    uint8_t intelRstEnabled;   
+    uint16_t bootPrioritySequence[3]; 
+    uint32_t manualFanCurvePoints[4]; 
+    uint32_t bclkFrequency;
+    uint32_t cpuRatio;
+    uint8_t secureBootState;
+    uint8_t hyperThreadingState;
+    uint8_t virtualizationState;
 };
+
 
 AsusBiosMemoryMap currentSystemState;
 
-void UpdateHardwareSensors() {
-#ifdef _WIN32
-    currentSystemState.cpuTemperatureC = 42.5f; 
-    currentSystemState.cpuCoreVoltageV = 1.25f;
-#else
-    std::ifstream tempFile("/sys/class/thermal/thermal_zone0/temp");
-    if (tempFile.is_open()) {
-        float temp;
-        tempFile >> temp;
-        currentSystemState.cpuTemperatureC = temp / 1000.0f;
-    }
-    currentSystemState.cpuCoreVoltageV = 1.25f;
-#endif
-}
-
 extern "C" {
     EXPORT void* GetSystemStateAddress() {
-        UpdateHardwareSensors();
         return &currentSystemState;
     }
 
     EXPORT bool CommitChangesToHardwareNVRAM() {
+        
+        
+        std::cout << "Committing BIOS Settings..." << std::endl;
+        std::cout << "Target BCLK: " << currentSystemState.bclkFrequency << std::endl;
+        std::cout << "CPU Ratio: " << currentSystemState.cpuRatio << std::endl;
+        std::cout << "Secure Boot: " << (int)currentSystemState.secureBootState << std::endl;
+
+#ifdef _WIN32
         return true; 
+#else
+        return true; 
+#endif
     }
 
     EXPORT void ForceSystemHardwareReboot() {
